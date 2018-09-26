a6bc5b5a0a4f866eaee4b032e308f2561d6d4399
donyahojabr <donya.hojabr@mail.mcgill.ca>
2018-09-26 19:20:39 -0400

    Add files via upload
    Added script to generate database and corresponding tables

---
+++ src/main/resources/dbo.sql
@@ -0,0 +1,71 @@
+
+
+CREATE DATABASE dbo;
+
+use dbo;
+
+CREATE TABLE `Users` (
+  `UserID` int(11) NOT NULL,
+  `Username` varchar(45) DEFAULT NULL,
+  `Email` varchar(45) DEFAULT NULL,
+  `Password` varchar(45) DEFAULT NULL,
+  `Rating` int(11) DEFAULT NULL,
+  `numRides` int(11) DEFAULT NULL,
+  `Role` int(11) DEFAULT NULL,
+  `Status` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`UserID`),
+  UNIQUE KEY `UserID_UNIQUE` (`UserID`),
+  UNIQUE KEY `Username_UNIQUE` (`Username`)
+);
+
+CREATE TABLE `Cars` (
+  `CarID` int(11) NOT NULL,
+  `Make` varchar(45) DEFAULT NULL,
+  `Model` varchar(45) DEFAULT NULL,
+  `Year` int(11) DEFAULT NULL,
+  `numSeats` int(11) DEFAULT NULL,
+  `LicensePlate` varchar(45) DEFAULT NULL,
+  `FK_UserID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`CarID`),
+  UNIQUE KEY `CarID_UNIQUE` (`CarID`)
+);
+
+CREATE TABLE `Legs` (
+  `LegID` int(11) NOT NULL,
+  `Start` varchar(45) DEFAULT NULL,
+  `End` varchar(45) DEFAULT NULL,
+  `Price` int(11) DEFAULT NULL,
+  `NumSeats` int(11) DEFAULT NULL,
+  `FK_TripID` int(11) NOT NULL,
+  PRIMARY KEY (`LegID`),
+  UNIQUE KEY `LegID_UNIQUE` (`LegID`)
+);
+
+
+CREATE TABLE `PassengerTrips` (
+  `PassengerTripID` int(11) NOT NULL AUTO_INCREMENT,
+  `FK_TripID` int(11) DEFAULT NULL,
+  `FK_UserID` int(11) DEFAULT NULL,
+  `Price` int(11) DEFAULT NULL,
+  PRIMARY KEY (`PassengerTripID`),
+  UNIQUE KEY `PassengerTripID_UNIQUE` (`PassengerTripID`)
+);
+
+
+CREATE TABLE `Trips` (
+  `TripID` int(11) NOT NULL AUTO_INCREMENT,
+  `Date` datetime DEFAULT NULL,
+  `Time` int(11) DEFAULT NULL,
+  `FK_UserID` int(11) DEFAULT NULL,
+  `FK_CarID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`TripID`),
+  UNIQUE KEY `TripID_UNIQUE` (`TripID`),
+  KEY `FK_CarID_idx` (`FK_CarID`),
+  KEY `FK_UserID_idx` (`FK_UserID`)
+);
+
+
+
+
+
+
