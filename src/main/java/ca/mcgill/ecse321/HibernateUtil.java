d7d4beb32a5c07555a51a3783d383d3a330959f6
Dylan Havelock <dylanhavelock@Dylans-MacBook-Pro-2.local>
2018-10-01 15:31:49 -0400

    Changes to User.java, HibernateUtil, UserController. Started updatePassenger method

--- src/main/java/ca/mcgill/ecse321/HibernateUtil.java
+++ src/main/java/ca/mcgill/ecse321/HibernateUtil.java
@@ -2,27 +2,26 @@ package ca.mcgill.ecse321;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.boot.MetadataSources;
-
+import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
 public class HibernateUtil {
 
-    private static final SessionFactory sessionFactory;
-
-    static {
-        final ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().configure().build();
-        sessionFactory = new MetadataSources(serviceRegistry).buildMetadata().buildSessionFactory();
+    private static SessionFactory sessionFactory;
+    private static ServiceRegistry serviceRegistry;
+    
+    static void createSessionFactory() {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        serviceRegistry = new StandardServiceRegistryBuilder().configure().build();
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
     }
 
     public static Session getSession() {
+        if (sessionFactory == null) {
+            createSessionFactory();
+        }
         return sessionFactory.openSession();
-    }
-
-    /* I wrote this method, the above was from McIntosh example */
-    public static void closeSession() {
-        sessionFactory.close();
     }
 }
