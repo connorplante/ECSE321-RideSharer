a303e2fb4fa7c1551ef0872b757f32e063fe6e15
Dylan Havelock <dylanhavelock@Dylans-MacBook-Pro-2.local>
2018-10-01 10:37:24 -0400

    Added createDriver controller method

--- src/main/java/ca/mcgill/ecse321/controller/UserController.java
+++ src/main/java/ca/mcgill/ecse321/controller/UserController.java
@@ -4,6 +4,7 @@ import ca.mcgill.ecse321.HibernateUtil;
 import org.hibernate.Session;
 import org.springframework.web.bind.annotation.*;
 import ca.mcgill.ecse321.model.Passenger;
+import ca.mcgill.ecse321.model.Driver;
 
 @RestController
 @RequestMapping("/User")
@@ -18,7 +19,7 @@ public class UserController {
      * @param lastName
      * @param email
      * @param phoneNumber
-     * @return an object of type Passenger
+     * @return Passenger passenger
      */
     @RequestMapping("/createPassenger")
     public Passenger createPassenger (@RequestParam(value="username") String username,@RequestParam(value="password") String password,
@@ -34,6 +35,33 @@ public class UserController {
 
         return passenger;
     }
+
+    /** 
+    * Method to create a user of type Driver
+    * Use the url /User/createDriver
+    * @param username
+    * @param password
+    * @param firstName
+    * @param lastName
+    * @param email
+    * @param phoneNumber
+    * @return Driver driver
+    */
+   @RequestMapping("/createDriver")
+   public Driver createDriver (@RequestParam(value="username") String username,@RequestParam(value="password") String password,
+   @RequestParam(value="firstName") String firstName, @RequestParam(value="lastName") String lastName,@RequestParam(value="email") String email,
+   @RequestParam(value="phoneNumber") String phoneNumber) {
+
+       Driver driver =  new Driver(username, password, firstName, lastName, email, phoneNumber, true, 0, 0);
+       Session session = HibernateUtil.getSession();
+       session.beginTransaction();
+       session.saveOrUpdate(driver);
+       session.getTransaction().commit();
+       session.close();
+
+       return driver;
+   }
+
 }
 
 
