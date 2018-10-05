5066ce90b47c00d3ee07c84c4ff067bf5a9d3d6a
mjtucker <mary.tucker@mail.mcgill.ca>
2018-10-05 14:39:39 -0400

    Commented TripController file, added in check for errors, Trip constructor takes int to represent time

--- src/main/java/ca/mcgill/ecse321/controller/TripController.java
+++ src/main/java/ca/mcgill/ecse321/controller/TripController.java
@@ -1,60 +1,81 @@
 package ca.mcgill.ecse321.controller;
 
 import ca.mcgill.ecse321.HibernateUtil;
-
 import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.springframework.web.bind.annotation.*;
 import ca.mcgill.ecse321.model.*;
-
 import java.sql.Date;
 import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
-
-
 import org.springframework.web.bind.annotation.RestController;
-
 import ca.mcgill.ecse321.model.*;
 import ca.mcgill.ecse321.model.Trip.Status;
+import ca.mcgill.ecse321.controller.InvalidInputException;
 
 @RestController
 @RequestMapping("/Trip")
 public class TripController {
 
-
     public TripController(){}
 
+    /**
+     * Method to create a Trip 
+     * Use the url /Trip/createTrip
+     * @param start
+     * @param end
+     * @param date
+     * @param time
+     * @param email
+     * @param username 
+     * @param carID
+     * @param numSeats
+     * @param stops 
+     * @param prices 
+     * @return Trip trip
+     */
+    
     @RequestMapping("/createTrip")
     public Trip createTrip(@RequestParam(value="start")String start, @RequestParam(value="end")String end, 
     @RequestParam(value="date")Date date, @RequestParam(value="time")int time,
-    @RequestParam(value="username") String username, @RequestParam(value="carID")int carID, int numSeats, @RequestParam List<String> stops, @RequestParam List<Double> prices) {
-       
-        Time time1 = null;
+    @RequestParam(value="username") String username, @RequestParam(value="carID")int carID, int numSeats, @RequestParam List<String> stops, @RequestParam List<Double> prices) throws InvalidInputException{
+
+        String error = "";
         
+        //Check if input values of start and end are equal to eachother 
+        if (start.equals(end)){
+            error = "This end point of a trip cannot be the same as the start point";
+        }
+        //If the error string contains an error, throw the error and end the method
+        if (error.length() > 0) {
+			throw new InvalidInputException(error.trim());
+		}
+
+        //Begin Session
         Session session = HibernateUtil.getSession();
         session.beginTransaction();
        
-      
+        //Access Driver object from database 
         Driver driver = (Driver) session.byNaturalId( User.class ).using( "username", username ).load();
         
-        
-        //access Car object 
+        //Access Car object from database 
         Car car = (Car) session.load(Car.class, carID);
-       
-
-        
            
-            Trip trip = new Trip(start, end, date, time1, Status.Scheduled, driver, car);
+        //Create a Trip Object 
+        Trip trip = new Trip(start, end, date, time, Status.Scheduled, driver, car);
             
-            session.save(trip);
-            session.getTransaction().commit();
-            session.close();
+        session.save(trip);
+        session.getTransaction().commit();
+        session.close();
             
-           
-            int counter = 0;
-            int sizePriceList = prices.size();
+        //Initialize a counter to be used in a for loop
+        int counter = 0;
 
+        //Defined sizePriceList as the size of the list of prices taken in as an argument 
+        int sizePriceList = prices.size();
+
+        //List trough all stops inputted by user and create a leg corresponding to the start, end, price, numSeats, and trip
             for (String stop: stops){
                 if ( counter < sizePriceList){
                 double price = prices.get(counter);
@@ -64,52 +85,65 @@ public class TripController {
                 trip.addLeg(leg);
                 counter++; 
                 }
-            }
-           
+            }  
         
-
+        //Return the created trip
         return trip;
-
-
     }
-    
-
-
-
+    /**
+     * Method to cancel a Trip 
+     * Use the url /Trip/cancelTrip
+     * @param tripID
+     * @return Trip trip
+     */
     @RequestMapping("/cancelTrip")
     public Trip cancelTrip(@RequestParam(value="tripID")int tripID){
-        Session session = HibernateUtil.getSession();
-            session.beginTransaction();
-
-        Trip trip = (Trip) session.load(Trip.class, tripID);
-        
-                trip.setTripStatus(Status.Cancelled);
-            
-            session.saveOrUpdate(trip);
-            session.getTransaction().commit();
-            session.close();
-        return trip;
-    }
-
-
-
-
-    public Leg createLeg(String pointA, String pointB, Double price, int numSeats, Trip trip){
-
-        
+    
+        //Begin Session
         Session session = HibernateUtil.getSession();
         session.beginTransaction();
 
+        //Access Trip object from the database 
+        Trip trip = (Trip) session.load(Trip.class, tripID);
+        
+        //Set the status of trip to cancelled
+        trip.setTripStatus(Status.Cancelled);
+            
+        //Save updates to database and close session
+        session.saveOrUpdate(trip);
+        session.getTransaction().commit();
+        session.close();
+      
+        //Return Trip
+        return trip;
+    }
+/**
+     * Method to Create a Leg
+     * Helper function used in the createTrip method 
+     * @param pointA 
+     * @param pointB 
+     * @param price 
+     * @param numSeats 
+     * @param trip
+     * @return Leg leg
+     */
+
+    public Leg createLeg(String pointA, String pointB, Double price, int numSeats, Trip trip){
+        
+        //Begin Session
+        Session session = HibernateUtil.getSession();
+        session.beginTransaction();
+
+        //Create new Leg object 
         Leg leg = new Leg(pointA, pointB, price, numSeats, trip);
         
-            session.save(leg);
-            session.getTransaction().commit();
-            session.close();
+        //Save to database and close session
+        session.save(leg);
+        session.getTransaction().commit();
+        session.close();
         
-        
+        //Return Leg 
         return leg;
-    
-
     }
 
     /**
