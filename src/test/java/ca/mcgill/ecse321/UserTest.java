acedd6d244c17fa975b4e5b2b7ff5ffd8466ea93
Connor Plante <connor.plante@mail.mcgill.ca>
2018-10-05 23:31:17 -0400

    added createDriver Test

--- src/test/java/ca/mcgill/ecse321/UserTest.java
+++ src/test/java/ca/mcgill/ecse321/UserTest.java
@@ -1,11 +1,13 @@
 package ca.mcgill.ecse321;
 import org.junit.Test;
 
+import ca.mcgill.ecse321.controller.UserController;
 import ca.mcgill.ecse321.model.*;
 
 import static org.junit.Assert.*;
 
 import org.hibernate.Session;
+import org.junit.Before;
 import org.junit.Rule;
 
 public class UserTest {
@@ -13,11 +15,21 @@ public class UserTest {
     @Rule 
     public final SessionFactoryRule sf = new SessionFactoryRule();
 
+    public UserController tUserController;
+
+    @Before
+    public void before() {
+        tUserController = new UserController();
+        tUserController.changeSession(sf.getSession());
+    }
+
     @Test
     public void returnsUser() {
 
         //  Arrange
         Session session = sf.getSession();
+
+        sf.beginTransaction();
 
         String username = "tUsername";
         String email = "test@test.com";
@@ -50,6 +62,25 @@ public class UserTest {
         assertEquals(returnedUser.getEmail(), email);
         assertEquals(returnedUser.getFirstName(), firstName);
         assertEquals(returnedUser.getLastName(), lastName);
+    }
+
+    @Test
+    public void createDriverTest() {
+        //  Arrange 
+        Session session = sf.getSession();
+
+        String username = "tUsername";
+        String email = "test@test.com";
+        String firstName = "tFirstName";
+        String lastName = "tLastName";
+        String password = "p";
+        String phone = "1";
+
+        Driver driver = tUserController.createDriver(username, password, firstName, lastName, email, phone);
+        User reUser = (User) session.byNaturalId(User.class).using("username", username).load();;
+
+        assertNotNull(reUser);
+
     }
 
 }
