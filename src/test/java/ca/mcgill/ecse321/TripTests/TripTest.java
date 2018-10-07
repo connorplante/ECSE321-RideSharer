f566063d1bd41f71ff0f885502fcf399c1031638
Connor Plante <connor.plante@mail.mcgill.ca>
2018-10-07 19:15:41 -0400

    Added cancelTrip and CompleteTrip tests

--- src/test/java/ca/mcgill/ecse321/TripTests/TripTest.java
+++ src/test/java/ca/mcgill/ecse321/TripTests/TripTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import ca.mcgill.ecse321.SessionFactoryRule;
 import ca.mcgill.ecse321.controller.*;
 import ca.mcgill.ecse321.model.*;
+import ca.mcgill.ecse321.model.Trip.Status;
 
 import static org.junit.Assert.*;
 
@@ -70,4 +71,92 @@ public class TripTest {
         assertNotNull(tTrip);
         assertEquals(result, tTrip.toString());
     }
+
+    //  Need Fix
+    @Test
+    public void cancelTripTest() {
+        //  Arrange 
+        Session session = sf.getSession();
+        sf.beginTransaction();
+
+        String username = "tDriver";
+
+        Driver tDriver = new Driver(username, "tPassword", "tFirstName", "tLastName", "tEmail", "tPhone", true, 0, 0);
+    
+        session.save(tDriver);
+
+        Car tCar = new Car("tMake", "tModel", 2000, 6, "tPlate", tDriver);
+
+        session.save(tCar);
+
+        String start = "tStart";
+        String end = "tEnd";
+        Date date = new Date(0);
+        int time = 1;
+        List<String> stops = new ArrayList<String>();
+        stops.add(start);
+        stops.add("tStop");
+        stops.add(end);
+        List<Double> prices = new ArrayList<Double>();
+        prices.add(14.0);
+        prices.add(19.0);
+        Trip tTrip = new Trip(start, end, date, time, Status.Scheduled, tDriver, tCar);
+
+        session.save(tTrip);
+
+        sf.commit();
+
+        //  Act
+        Boolean flag = tTripController.cancelTrip(1);
+        Trip reTrip = tTripController.getTripByID(1);
+
+        //  Assert
+        assertTrue(flag);
+        assertEquals(Status.Cancelled, reTrip.getTripStatus());
+    }
+
+    // Need Fix
+    @Test
+    public void completeTripTest() {
+        //  Arrange 
+        Session session = sf.getSession();
+        sf.beginTransaction();
+
+        String username = "tDriver";
+
+        Driver tDriver = new Driver(username, "tPassword", "tFirstName", "tLastName", "tEmail", "tPhone", true, 0, 0);
+    
+        session.save(tDriver);
+
+        Car tCar = new Car("tMake", "tModel", 2000, 6, "tPlate", tDriver);
+
+        session.save(tCar);
+
+        String start = "tStart";
+        String end = "tEnd";
+        Date date = new Date(0);
+        int time = 1;
+        List<String> stops = new ArrayList<String>();
+        stops.add(start);
+        stops.add("tStop");
+        stops.add(end);
+        List<Double> prices = new ArrayList<Double>();
+        prices.add(14.0);
+        prices.add(19.0);
+        Trip tTrip = new Trip(start, end, date, time, Status.Scheduled, tDriver, tCar);
+
+        session.save(tTrip);
+
+        sf.commit();
+
+        //  Act
+        Boolean flag = tTripController.completeTrip(1, username);
+        Trip reTrip = tTripController.getTripByID(1);
+
+        //  Assert
+        assertTrue(flag);
+        assertEquals(Status.Completed, reTrip.getTripStatus());
+    }
+
+    
 }
