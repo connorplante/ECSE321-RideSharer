eabd11f276f9a25f0fb7eb29f2bd183ea6fc9898
Dylan Havelock <dylanhavelock@Dylans-MacBook-Pro-2.local>
2018-10-31 16:28:24 -0400

    HUUUGGEEE commit

---
+++ RideSharerPassenger/app/src/main/java/ca/mcgill/ecse321/ridesharerpassenger/MyAdapter.java
@@ -0,0 +1,12 @@
+package ca.mcgill.ecse321.ridesharerpassenger;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class MyAdapter {
+
+    // override other abstract methods here
+
+}
---
+++ RideSharerPassenger/app/src/main/java/ca/mcgill/ecse321/ridesharerpassenger/ViewMapActivity.java
@@ -0,0 +1,41 @@
+package ca.mcgill.ecse321.ridesharerpassenger;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class ViewMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_map);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+
+
+    }
+
+
+}
