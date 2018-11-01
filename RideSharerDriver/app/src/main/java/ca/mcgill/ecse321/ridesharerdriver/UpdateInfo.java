1a76b74114601ec169f7401ba8ab23c23c316568
cattanisam <samantha.cattani@mail.mcgill.ca>
2018-11-01 11:42:46 -0400

    update profiles supported

--- RideSharerDriver/app/src/main/java/ca/mcgill/ecse321/ridesharerdriver/UpdateInfo.java
+++ RideSharerDriver/app/src/main/java/ca/mcgill/ecse321/ridesharerdriver/UpdateInfo.java
@@ -10,9 +10,26 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+
+import cz.msebera.android.httpclient.Header;
+
 public class UpdateInfo extends AppCompatActivity {
 
     String error = "";
+
+    String firstName;
+    String lastName;
+    String email;
+    String phoneNum;
+
+    public static final String FIRSTNAME = "ca.mcgill.ecse321.ridesharerdriver.firstName";
+    public static final String LASTNAME = "ca.mcgill.ecse321.ridesharerdriver.lastName";
+    public static final String EMAIL  = "ca.mcgill.ecse321.ridesharerdriver.email";
+    public static final String PHONE = "ca.mcgill.ecse321.ridesharerdriver.phone";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +46,31 @@ public class UpdateInfo extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        if(getIntent().hasExtra(FIRSTNAME)){
+            firstName = getIntent().getStringExtra(FIRSTNAME);
+        }
+
+        if(getIntent().hasExtra(LASTNAME)){
+            lastName = getIntent().getStringExtra(LASTNAME);
+        }
+
+        if(getIntent().hasExtra(EMAIL)){
+            email = getIntent().getStringExtra(EMAIL);
+        }
+
+        if(getIntent().hasExtra(PHONE)){
+            phoneNum = getIntent().getStringExtra(PHONE);
+        }
+
+        final TextView a = (TextView) findViewById(R.id.firstNameText);
+        a.setHint(firstName);
+        final TextView b = (TextView) findViewById(R.id.lastNameText);
+        b.setHint(lastName);
+        final TextView c = (TextView) findViewById(R.id.emailText);
+        c.setHint(email);
+        final TextView d = (TextView) findViewById(R.id.phoneText);
+        d.setHint(phoneNum);
     }
 
     @Override
@@ -65,5 +107,72 @@ public class UpdateInfo extends AppCompatActivity {
         }
 
     }
+
+    public void updateInfo(View v){
+        error = "";
+        final TextView ta = (TextView) findViewById(R.id.firstNameText);
+        final TextView tb = (TextView) findViewById(R.id.lastNameText);
+        final TextView tc = (TextView) findViewById(R.id.emailText);
+        final TextView td = (TextView) findViewById(R.id.phoneText);
+
+        String firstName = "";
+        String lastName = "";
+        String email = "";
+        String phone = "";
+
+        if(ta.getText().toString().equals("")){
+            firstName = ta.getHint().toString();
+        }else{
+            firstName = ta.getText().toString();
+        }
+
+        if(tb.getText().toString().equals("")){
+            lastName = tb.getHint().toString();
+        }else{
+            lastName = tb.getText().toString();
+        }
+
+        if(tc.getText().toString().equals("")){
+            email = tc.getHint().toString();
+        }else{
+            email = tc.getText().toString();
+        }
+
+        if(td.getText().toString().equals("")){
+            phone = td.getHint().toString();
+        }else{
+            phone = td.getText().toString();
+        }
+
+        final String finalfirstName = firstName;
+        final String finalLastName = lastName;
+        final String finalEmail = email;
+        final String finalPhone = phone;
+
+
+        String username = "samcattani";
+
+        String url = "User/updateUserInfo?username=" + username + "&firstName=" + firstName + "&lastName=" + lastName + "&email=" + email + "&phoneNumber=" + phone;
+
+        HttpUtils.post(url, new RequestParams(), new JsonHttpResponseHandler(){
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response){
+
+                ta.setHint(finalfirstName);
+                tb.setHint(finalLastName);
+                tc.setHint(finalEmail);
+                td.setHint(finalPhone);
+
+                ta.setText("");
+                tb.setText("");
+                tc.setText("");
+                td.setText("");
+
+            }
+
+        });
+    }
+
 
 }
