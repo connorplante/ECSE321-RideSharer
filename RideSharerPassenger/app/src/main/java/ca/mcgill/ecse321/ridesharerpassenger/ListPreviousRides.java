d282d994f2b3c0c6e3edf86aa9bf0aa7724056fd
annejuliec <43216613+annejuliec@users.noreply.github.com>
2018-11-01 21:27:38 -0400

    updated working version

--- RideSharerPassenger/app/src/main/java/ca/mcgill/ecse321/ridesharerpassenger/ListPreviousRides.java
+++ RideSharerPassenger/app/src/main/java/ca/mcgill/ecse321/ridesharerpassenger/ListPreviousRides.java
@@ -1,5 +1,6 @@
 package ca.mcgill.ecse321.ridesharerpassenger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,25 +9,175 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
 
 public class ListPreviousRides extends AppCompatActivity {
 
     String error = "";
 
+    ArrayList<String> trips;
+    ArrayList<String> places;
+    ArrayList<String> days;
+    ArrayList<String> times;
+    ArrayList<String> numSeats;
+
+    public static final String TRIPS = "ca.mcgill.ecse321.ridesharerpassenger.trips";
+    public static final String PLACES = "ca.mcgill.ecse321.ridesharerpassenger.places";
+    public static final String DAYS  = "ca.mcgill.ecse321.ridesharerpassenger.days";
+    public static final String TIMES = "ca.mcgill.ecse321.ridesharerpassenger.times";
+    public static final String NUMSEATS = "ca.mcgill.ecse321.ridesharerpassenger.numSeats";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_previous_rides);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
+        setContentView(R.layout.content_list_previous_rides);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        if (getIntent().hasExtra(TRIPS)) {
+            trips = getIntent().getStringArrayListExtra(TRIPS);
+        } else {
+            throw new IllegalArgumentException("cannot find extras " + TRIPS);
+        }
+
+        if (getIntent().hasExtra(PLACES)) {
+            places = getIntent().getStringArrayListExtra(PLACES);
+        } else {
+            throw new IllegalArgumentException("cannot find extras " + PLACES);
+        }
+
+        if (getIntent().hasExtra(DAYS)) {
+            days = getIntent().getStringArrayListExtra(DAYS);
+        } else {
+            throw new IllegalArgumentException("cannot find extras "+ DAYS);
+        }
+
+        if (getIntent().hasExtra(TIMES)) {
+            times = getIntent().getStringArrayListExtra(TIMES);
+        } else {
+            throw new IllegalArgumentException("cannot find extras "+ TIMES);
+        }
+
+        if (getIntent().hasExtra(NUMSEATS)) {
+            numSeats = getIntent().getStringArrayListExtra(NUMSEATS);
+        } else {
+            throw new IllegalArgumentException("cannot find extras " + NUMSEATS);
+        }
+
+        System.out.println("ListPreviousRides:");
+        System.out.println(places);
+        System.out.println(days);
+        System.out.println(times);
+        System.out.println(numSeats);
+
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        ListPreviousRides.CustomAdapter customAdapter = new ListPreviousRides.CustomAdapter();
+        listView.setAdapter(customAdapter);
+    }
+
+        class CustomAdapter extends BaseAdapter {
+
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public int getCount() {
+                return trips.size();
             }
+
+            @Override
+            public Object getItem(int position) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return 0;
+            }
+
+            @Override
+            public View getView(int position, View view, ViewGroup parent) {
+                view = getLayoutInflater().inflate(R.layout.custom_previousrides, null);
+                TextView tripIdView = (TextView) view.findViewById(R.id.textTripID);
+                TextView routeView = (TextView) view.findViewById(R.id.textView7);
+                TextView timeView = (TextView) view.findViewById(R.id.textView8);
+                TextView dateView = (TextView) view.findViewById(R.id.textView9);
+                TextView numSeatsView = (TextView) view.findViewById(R.id.textView10);
+                //TextView rateDriver = (TextView) view.findViewById(R.id.textView11);
+
+                if (trips.size() == 0 || places.size() == 0) {
+                    tripIdView.setText("");
+                    routeView.setText("No trips ended");
+                    dateView.setText("");
+                    timeView.setText("");
+                    numSeatsView.setText("");
+                    //rateDriver.setText("");
+                    return view;
+                }
+
+                tripIdView.setText("Trip " + trips.get(position));
+                routeView.setText(places.get(position));
+                dateView.setText("Date: " + days.get(position));
+                timeView.setText("Time: " + times.get(position));
+                numSeatsView.setText("Num Seats: " + numSeats.get(position));
+                //rateDriver.setText("Rate your driver");
+
+                return view;
+
+            }
+
+        }
+    public void viewRateDriver (View v) {
+        error = "";
+
+        TextView tripIdView = (TextView) ListPreviousRides.this.findViewById(R.id.textTripID);
+        String tripID = tripIdView.getText().toString();
+        tripID = tripID.substring(5);
+
+        System.out.print("\n tripID is :" + tripID);
+        String url = "/User/showDriverForTrip?TripID="+tripID;
+        HttpUtils.post(url, new RequestParams(), new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                System.out.println("\n success!");
+                System.out.println("\n response:" + response.toString());
+                //ArrayList<String> driver = new ArrayList<String>();
+
+                String s = response.toString();
+                System.out.print("s is " + s);
+
+                s = s.replaceAll("\\[", "");
+                s = s.replaceAll("\\]", ",");
+                System.out.println("s is now " + s);
+
+                viewRateDriver(s);
+                }
+
+            @Override
+            public void onFinish() {
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    error += errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error += e.getMessage();
+                }
+                refreshErrorMessage();
+            }
+
         });
     }
 
@@ -52,4 +203,27 @@ public class ListPreviousRides extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+   public void viewRateDriver(String driver){
+        Intent intent = new Intent(this, RateDriver.class);
+        Bundle b = new Bundle();
+        b.putStringArrayList(ListPreviousRides.TRIPS, trips);
+        b.putString(RateDriver.DRIVER, driver);
+        System.out.println("=======================================");
+
+        intent.putExtras(b);
+        startActivity(intent);
+    }
+
+    private void refreshErrorMessage() {
+        // set the error message
+        TextView tvError = (TextView) findViewById(R.id.error);
+        tvError.setText(error);
+
+        if (error == null || error.length() == 0) {
+            tvError.setVisibility(View.GONE);
+        } else {
+            tvError.setVisibility(View.VISIBLE);
+        }
+
+    }
 }
